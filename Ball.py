d7f166c7626c2737ff14037808b50fdebdae32aa
vm01knac <vm01knac@siena.edu>
2019-02-07 16:02:13 -0500

    First Commit

---
+++ Ball.py
@@ -0,0 +1,61 @@
+﻿# File: Ball.py
+
+import viz
+import math
+import random
+
+# Ball class inherits event handling methods from viz.EventClass
+class Ball:
+
+	# Constructor 
+	def __init__(self):
+		
+		# Initialize Ball Instance Variables
+		# radius 
+		self.radius = 10
+		# number of sides of regular polygon representing the ball
+		self.sides = 50
+		
+		degree = random.randint(0, 360)
+		# velocity vector describing its direction and speed
+		self.vx = 2*math.cos(math.radians(degree))
+		self.vy = 2*math.sin(math.radians(degree))
+		# center location 
+		self.x = 0
+		self.y = 0
+		
+		# create layer for a circle, centered at (0,0)
+		viz.startLayer(viz.POLYGON)
+		viz.vertexColor(0,1,0)
+		for i in range(0, 360, (360/self.sides)):
+			x = math.cos( math.radians(i) ) * self.radius
+			y = math.sin( math.radians(i) ) * self.radius
+			viz.vertex(x, y)
+		# saves layer of vertices in instance variable called vertices
+		self.vertices = viz.endLayer()
+		
+	def getX(self):
+		return self.x
+		
+	def getY(self):
+		return self.y
+		
+	def getVX(self):
+		return self.vx
+		
+	def getVY(self):
+		return self.vy
+		
+	def setXY(self,x,y):
+		self.x = x
+		self.y = y
+		mat = viz.Matrix()
+		mat.postTrans(self.x, self.y)
+		self.vertices.setMatrix(mat)
+		
+	def setVXVY(self,vx,vy):
+		self.vx = vx
+		self.vy = vy
+		
+		
+	
---
+++ Controller.py
@@ -0,0 +1,49 @@
+﻿# File: Controller.py
+
+import viz
+from Ball import *
+
+# Controller class inherits event handling methods from viz.EventClass
+class Controller(viz.EventClass):
+	
+	def __init__(self):
+		
+		# must call constructor of EventClass first!!
+		viz.EventClass.__init__(self)
+		self.balls = list()
+		self.timer = False
+		#self.ball = Ball()
+		self.callback(viz.TIMER_EVENT,self.onTimer)
+		self.callback(viz.KEYDOWN_EVENT, self.onKeyDown)
+		
+	def onTimer(self,num):
+		#print("Timer " + str(num))
+		for ball in self.balls:
+			if (ball.getY() + ball.radius >= 100 or  ball.getY() - ball.radius <= -100):
+				print("X Axis Wall")
+				ball.setVXVY(ball.getVX(), -ball.getVY())
+				viz.playSound("cartoon053.wav", viz.PLAY)
+			if(ball.getX() + ball.radius >= 100) or (ball.getX() - ball.radius <= -100):
+				print("Y Axis Wall")
+				ball.setVXVY(-ball.getVX(), ball.getVY())
+				viz.playSound("cartoon053.wav", viz.PLAY)
+			ball.setXY(ball.getVX() + ball.getX(), ball.getVY() + ball.getY())
+		#print("X: "+ str(self.ball.getX()))
+		#print("Y: "+ str(self.ball.getY()))
+		
+	def onKeyDown(self, key):
+			if(key == " "):
+				self.balls.append(Ball())
+				
+				if not self.timer:
+					self.starttimer(1, 1/25, viz.FOREVER)
+					self.timer = True
+					
+			if key == viz.KEY_DOWN:
+				for ball in self.balls:
+					ball.setVXVY(ball.getVX() * .95, ball.getVY()*.95)
+			if key == viz.KEY_UP:
+				for ball in self.balls:
+					ball.setVXVY(ball.getVX() * 1.05, ball.getVY()*1.05)
+	
+	
